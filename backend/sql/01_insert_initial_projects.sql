da6dbc22d53eee5cd485990604902d56634f3427
Ilia Kuznetsov <ilia.kuznetsov.work@gmail.com>
2026-02-07 22:53:17 +0100

    Add SQL scripts directory with initial projects insert script

---
+++ backend/sql/01_insert_initial_projects.sql
@@ -0,0 +1,21 @@
+-- Insert initial projects into the projects table
+-- This script populates the database with common project categories for time tracking
+
+INSERT INTO projects (name) VALUES
+('Computer games'),
+('YouTube'),
+('Running'),
+('Fast walking'),
+('Films'),
+('Podcasts'),
+('Vibe coding'),
+('Telegram channel'),
+('Reading fiction'),
+('Reading tech books'),
+('Reading non-fiction'),
+('English'),
+('Diary'),
+('Meatings'),
+('Slow walking'),
+('Growth sessions');
+
